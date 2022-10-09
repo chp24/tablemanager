6a49241186312c8d1de0452ad81633a5ee3175f4
Chris <christopherhpena02@gmail.com>
2022-10-09 12:33:47 -0400

    added the get command

--- commands/getRows.js
+++ commands/getRows.js
@@ -1,3 +1,20 @@
-module.exports = function() {
-    console.log(arguments);
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('./db/local_db');
+
+module.exports = function(tableName, options) {
+    const columnNames = options.columnNames || "*";
+    const query = `select ${columnNames} from ${tableName}`;
+    const results = runQuery(query);
+    console.log(results);
 }
+
+function runQuery(query){
+    db.serialize(() => {
+        db.each(query, (err, row) => {
+            console.log(row);
+        }, ()=>{
+            db.close();
+        })
+    });
+}
+    
