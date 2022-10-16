52355b12404cd25aa4eca61ba93bf37b86c405a4
Chris <christopherhpena02@gmail.com>
2022-10-16 10:17:31 -0400

    added a temporary version of addcolumn

--- commands/addcolumn.js
+++ commands/addcolumn.js
@@ -1,3 +1,10 @@
-module.exports = function() {
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('./db/local_db');
+
+module.exports = async function(tableName, columnName) {
+    
+    console.log({tableName, columnName});
+    db.run(`ALTER TABLE ${tableName} ADD ${columnName} TEXT`);
+
     console.log("addColumn!")
 }
