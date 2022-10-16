7b058c6cbe9ed48bf33c7f964b81d87b9a1aa90f
Chris <christopherhpena02@gmail.com>
2022-10-16 10:09:09 -0400

    got addrow working for the most part

--- commands/addRow.js
+++ commands/addRow.js
@@ -5,14 +5,19 @@ module.exports = async function(tableName,options ) {
     //db.run(`INSERT INTO ${tableName} (${columnName}) VALUES(1)`);
 
     console.log({tableName, options});
-    console.log(options.values.split(","))
-
+    var columnsWithValues = options.values.split(",");
+    var columns=[];
+    var values=[];
     for(let i = 0; i < columnsWithValues.length; i++) {
         const data = columnsWithValues[i];
-        const columnDataArray = data.split("i");
+        const columnDataArray = data.split("=");
         const columnName = columnDataArray[0];
         const value = columnDataArray[1];
-        console.log({columnName,value});
-        //db.run(`INSERT INTO ${tableName} (${columnName}`)
+        columns[i] = columnName;
+        values[i]= value;
+        console.log( values);
     }
+    
+    db.run(`INSERT INTO ${tableName} (${columns}) VALUES(${values})`);
+
 }
