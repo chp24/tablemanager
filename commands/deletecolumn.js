bb6724b62ac6f98d200661b272dd3be81ce31e81
Chris <christopherhpena02@gmail.com>
2022-10-17 17:28:23 -0400

    got the delete column command to work

--- commands/deletecolumn.js
+++ commands/deletecolumn.js
@@ -1,3 +1,9 @@
-module.exports = function() {
-    console.log("deletecolumn!")
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('./db/local_db');
+
+module.exports = function(tableName, columnName) {
+    var query = (`ALTER TABLE ${tableName} DROP ${columnName}`);
+    //console.log(`ALTER TABLE ${tableName} DROP (${columnName})`);
+    db.run(query);
+    console.log("deletecolumn!");
 }
